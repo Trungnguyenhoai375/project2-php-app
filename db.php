82ea43bdb58053c09eba3327c6dc4302946940e4
HOAITRUNG NGUYEN <trungnguyenhoai375@gmail.com>
2025-12-18 21:17:50 +0700

    actionnnnnnnnnnn

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
+// Tắt hiện lỗi ra màn hình sau khi đã biết lỗi là gì để bảo mật
 error_reporting(E_ALL);
-ini_set('display_errors', 1); // Ép server hiện lỗi thay vì hiện số 500
+ini_set('display_errors', 1); 
 
 $host = "sql204.infinityfree.com"; //
 $user = "if0_40712621";            //
-$pass = "klQM2OM8uldYT66";         //
+$pass = "klQM2OM8uldYT66";         // Trung hãy copy lại từ nút "Con mắt" trong Account Details
 $db   = "if0_40712621_project2";   //
 
 $conn = mysqli_connect($host, $user, $pass, $db);
