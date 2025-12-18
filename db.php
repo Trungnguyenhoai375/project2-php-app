51b63135373ef15fca206d6a26f2492ce19f652c
HOAITRUNG NGUYEN <trungnguyenhoai375@gmail.com>
2025-12-18 21:16:25 +0700

    action view

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1); // Ép server hiện lỗi thay vì hiện số 500
+
 $host = "sql204.infinityfree.com"; //
 $user = "if0_40712621";            //
 $pass = "klQM2OM8uldYT66";         //
@@ -7,6 +10,6 @@ $db   = "if0_40712621_project2";   //
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Kết nối thất bại!");
+    die("Kết nối MySQL thất bại: " . mysqli_connect_error());
 }
 ?>
