12b45b985dd54b01083d35fd7ee6365b80f163c9
HOAITRUNG NGUYEN <trungnguyenhoai375@gmail.com>
2025-12-18 21:21:40 +0700

    action lan cuoi lam on

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
-// Tắt hiện lỗi ra màn hình sau khi đã biết lỗi là gì để bảo mật
 error_reporting(E_ALL);
 ini_set('display_errors', 1); 
 
 $host = "sql204.infinityfree.com"; //
 $user = "if0_40712621";            //
-$pass = "klQM2OM8uldYT66";         // Trung hãy copy lại từ nút "Con mắt" trong Account Details
+$pass = "kIQM2OM8uldYT66"; //
 $db   = "if0_40712621_project2";   //
 
 $conn = mysqli_connect($host, $user, $pass, $db);
